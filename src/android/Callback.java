5ff74ca1559dbcc8cfb5e2bb51de0b41b6620501
Jack Lu <jacklu@jacklu.me>
2017-08-08 21:04:15 +0800

    +start

--- src/android/Callback.java
+++ src/android/Callback.java
@@ -2,10 +2,14 @@ package com.youbanban.cordova.plugin;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.PluginResult;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Callback extends CordovaPlugin {
 
@@ -18,9 +22,22 @@ public boolean execute(String action, JSONArray args, CallbackContext callbackCo
 		return true;
 	}
 	else if (action.equals("start")) {
+		this.start();
 		return true;
 	}
 	return false;
+}
+
+private void start() {
+	Timer timer = new Timer();
+	timer.scheduleAtFixedRate(new TimerTask() {
+		@Override
+		public void run() {
+			PluginResult result = new PluginResult(PluginResult.Status.OK);
+			result.setKeepCallback(true);
+			Callback.this.callbackContext.sendPluginResult(result);
+		}
+	}, 1000, 1000);
 }
 
 }
